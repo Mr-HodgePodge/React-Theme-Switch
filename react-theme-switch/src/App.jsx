5d0d17625f3543d31660ac1b2ffd74906cfe9340
Dylan <dylan_krajnc@tutanota.com>
2023-10-06 22:44:32 +0100

    Vite Build
    - as npx create-react-app is now deprecated, changed initial project setup to make use of Vite

---
+++ react-theme-switch/src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import useLocalStorage from 'use-local-storage'
+
+function App() {
+  return (
+    <div className='login'>
+      <h1>Login</h1>
+      <div className='container'>
+        <div className='top'>
+          <i className='fab fa-google'></i>
+          <i className='fab fa-facebook-square'></i>
+          <i className='fab fa-linkedin'></i>
+          <i className='fab fa-twitter-square'></i>
+          <i className='fab fa-apple'></i>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
